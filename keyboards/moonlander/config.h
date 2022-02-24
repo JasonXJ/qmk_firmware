692922a9b37bd13c8a2c7e95f015dc3c5112dea7
Jason Lin <lxj@google.com>
2022-02-25 10:03:26 +1100

    define COMBO_TERM for moonlander

--- keyboards/moonlander/config.h
+++ keyboards/moonlander/config.h
@@ -59,6 +59,8 @@
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
+#define COMBO_TERM 15
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
